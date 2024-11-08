47df2a9bbe88595cb10273d6ccb9a8d493a0c055
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-07 23:22:02 -0500

    styled the nav: fixed position, imported logo, used flexbox for spacing, imported fonts

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,24 @@
 import React from "react";
-import "./navbar.css";
+import "./navbar.scss";
 import { Link } from "react-router-dom";
+import logo from "./../assets/logo.png";
 
 function NavComponent(props) {
 	return (
 		<nav className="NavComponent">
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/locations">Locations</Link>
-				</li>
-				<li>
-					<Link to="/events">Events</Link>
-				</li>
-			</ul>
+			<div className="Logo">
+				<Link to="/" className="nav-link">
+					<img id="logo-btn" title="logo.png" src={logo} />
+				</Link>
+			</div>
+			<div className="nav-links">
+				<Link to="/locations" className="nav-link">
+					Locations
+				</Link>
+				<Link to="/events" className="nav-link">
+					Events
+				</Link>
+			</div>
 		</nav>
 	);
 }

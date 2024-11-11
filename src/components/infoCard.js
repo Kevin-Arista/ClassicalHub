1e45ade67fbfd77e0858a0ce5659b2a64dbe127b
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-11 16:25:01 -0500

    created base template and minimal styling for non-profit about section

---
+++ src/components/infoCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./infoCard.scss";
+
+function InfoCard(props) {
+	return (
+		<div className="infoCard">
+			<h1>This is an info card</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+				placerat risus sed risus pharetra, vitae blandit purus suscipit. Integer
+				sagittis dictum venenatis. Fusce felis felis, sollicitudin eu magna vel,
+				laoreet porta quam. Mauris at pharetra lorem. Nulla enim mi, consequat
+				at neque at, suscipit blandit risus. Donec consectetur nisi cursus urna
+				tristique, eu dictum augue luctus. Donec condimentum est vitae interdum
+				pharetra. Fusce non nunc sit amet ipsum lacinia convallis. Pellentesque
+				nisl libero, consectetur pulvinar lorem a, luctus congue justo. Cras
+				placerat quam nec eros imperdiet consectetur. Cras a augue volutpat quam
+				consectetur placerat vel ut felis. Maecenas at mattis sapien. Morbi in
+				nulla nec massa porttitor convallis. Proin lobortis, mi ac cursus
+				egestas, lacus tortor egestas metus, eget commodo dui erat eu libero. In
+				id dignissim ex, vitae euismod quam. Interdum et malesuada fames ac ante
+				ipsum primis in faucibus.
+			</p>
+		</div>
+	);
+}
+
+export default InfoCard;

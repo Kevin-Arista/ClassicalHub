0056ad3176e1f1e7bd9ff8ba958b710d7c55052f
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-11 15:23:39 -0500

    created template bioCard and nested inside Home page

---
+++ src/components/bioCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./bioCard.scss";
+
+function BioCard(props) {
+	return (
+		<div className="card-container">
+			<h1>This is my Bio Card</h1>
+		</div>
+	);
+}
+
+export default BioCard;

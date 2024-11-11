cb54c8f31867a31fec1894763a2a8f3ec4fded29
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-11 16:26:03 -0500

    implemented MyMap iframe, began nesting infoCard components, spaced out using flex

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "./locations.css";
+import InfoCard from "../components/infoCard";
 
 function Locations(props) {
 	return (
 		<div className="Locations">
-			<h1>Locations Page</h1>
+			<div style={{ height: "15vh" }}></div>
+			<iframe
+				className="map-container"
+				title="map-container"
+				src="https://www.google.com/maps/d/u/0/embed?mid=14HR6Ov6_nQwviTEIL-HuIDPG4RVCNDY&ehbc=2E312F"></iframe>
+			<div className="info-cards-container">
+				<div className="card-row">
+					<InfoCard></InfoCard>
+					<InfoCard></InfoCard>
+				</div>
+				<div className="card-row">
+					<InfoCard></InfoCard>
+					<InfoCard></InfoCard>
+				</div>
+			</div>
+			<div className="card-row">
+				<InfoCard></InfoCard>
+				<InfoCard></InfoCard>
+			</div>
 		</div>
 	);
 }

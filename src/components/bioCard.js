c7edf729b10b72063d19158ae5cab4731549aa89
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-14 20:47:48 -0500

    styled bioCard on homepage

--- src/components/bioCard.js
+++ src/components/bioCard.js
@@ -3,8 +3,26 @@ import "./bioCard.scss";
 
 function BioCard(props) {
 	return (
-		<div className="card-container">
-			<h1>This is my Bio Card</h1>
+		<div className="bioCard">
+			<div className="card-container">
+				<div className="text-container">
+					<h1>Hello, </h1>
+					<h1>My name is John!</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamc. Lorem ipsum dolor
+						sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+						incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+						veniam, quis nostrud exercitation ullamc
+					</p>
+				</div>
+				<div className="vl"></div>
+				<img
+					className="bioPicture"
+					alt="profile_pic"
+					src="https://thenoblepaw.com/wp-content/uploads/2022/03/Dachshund.jpg"></img>
+			</div>
 		</div>
 	);
 }

befc3a519b5a8ed3d0a0e15d2766419851b1f428
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-15 16:26:43 -0500

    transfered pop-up animation and fixed styling

--- src/components/infoCard.js
+++ src/components/infoCard.js
@@ -1,26 +1,27 @@
 import React from "react";
 import "./infoCard.scss";
+import externalLink from "../assets/external-link.png";
+import phone from "../assets/phone-call.png";
 
 function InfoCard(props) {
 	return (
 		<div className="infoCard">
-			<h1>This is an info card</h1>
+			<h1>YOLA</h1>
+			<hr />
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
 				placerat risus sed risus pharetra, vitae blandit purus suscipit. Integer
 				sagittis dictum venenatis. Fusce felis felis, sollicitudin eu magna vel,
 				laoreet porta quam. Mauris at pharetra lorem. Nulla enim mi, consequat
 				at neque at, suscipit blandit risus. Donec consectetur nisi cursus urna
-				tristique, eu dictum augue luctus. Donec condimentum est vitae interdum
-				pharetra. Fusce non nunc sit amet ipsum lacinia convallis. Pellentesque
-				nisl libero, consectetur pulvinar lorem a, luctus congue justo. Cras
-				placerat quam nec eros imperdiet consectetur. Cras a augue volutpat quam
-				consectetur placerat vel ut felis. Maecenas at mattis sapien. Morbi in
-				nulla nec massa porttitor convallis. Proin lobortis, mi ac cursus
-				egestas, lacus tortor egestas metus, eget commodo dui erat eu libero. In
-				id dignissim ex, vitae euismod quam. Interdum et malesuada fames ac ante
-				ipsum primis in faucibus.
+				tristique, eu dictum augue luctus.
 			</p>
+			<div className="icon-row">
+				<a href="https://www.laphil.com/learn/yola/youth-orchestra-los-angeles">
+					<img src={externalLink} />
+				</a>
+				<img src={phone} />
+			</div>
 		</div>
 	);
 }

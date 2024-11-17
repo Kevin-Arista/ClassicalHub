86c745fa6740dbe5aea6790fbc81379962f62dcc
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-17 14:57:14 -0500

    rendered database objects using map()

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -3,26 +3,64 @@ import "./locations.css";
 import InfoCard from "../components/infoCard";
 
 function Locations(props) {
+	const nonProfits = [
+		{
+			name: "",
+			description: "",
+			link: "",
+			phone: "",
+		},
+		{
+			name: "",
+			description: "",
+			link: "",
+			phone: "",
+		},
+		{
+			name: "",
+			description: "",
+			link: "",
+			phone: "",
+		},
+		{
+			name: "",
+			description: "",
+			link: "",
+			phone: "",
+		},
+		{
+			name: "",
+			description: "",
+			link: "",
+			phone: "",
+		},
+		{
+			name: "",
+			description: "",
+			link: "",
+			phone: "",
+		},
+	];
+
 	return (
 		<div className="Locations">
 			<div style={{ height: "15vh" }}></div>
 			<iframe
+				title="mymap"
 				className="map-container"
 				src="https://www.google.com/maps/d/u/7/embed?mid=1NIg4BgSx5HuC3NKGj6psfsecvW4yDqg&ehbc=2E312F&noprof=1"
 				width="640"
 				height="480"></iframe>
 			<h1>Learn More Below!</h1>
 			<div className="info-cards-container">
-				<InfoCard></InfoCard>
-				<InfoCard></InfoCard>
-				<InfoCard></InfoCard>
-				<InfoCard></InfoCard>
-				<InfoCard></InfoCard>
-				<InfoCard></InfoCard>
-				<InfoCard></InfoCard>
-				<InfoCard></InfoCard>
-				<InfoCard></InfoCard>
-				<InfoCard></InfoCard>
+				{nonProfits.map((org, index) => (
+					<InfoCard
+						key={index}
+						orgName={org.name}
+						description={org.description}
+						link={org.link}
+						phone={org.phone}></InfoCard>
+				))}
 			</div>
 		</div>
 	);

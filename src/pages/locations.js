eab652668e9ddf345b87907c309b434c0eaaec03
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-17 13:41:55 -0500

    caption to read below

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -11,25 +11,16 @@ function Locations(props) {
 				src="https://www.google.com/maps/d/u/7/embed?mid=1NIg4BgSx5HuC3NKGj6psfsecvW4yDqg&ehbc=2E312F&noprof=1"
 				width="640"
 				height="480"></iframe>
+			<h1>Learn More Below!</h1>
 			<div className="info-cards-container">
-				<div className="card-row">
-					<InfoCard></InfoCard>
-					<InfoCard></InfoCard>
-				</div>
-				<div className="card-row">
-					<InfoCard></InfoCard>
-					<InfoCard></InfoCard>
-				</div>
-			</div>
-			<div className="card-row">
 				<InfoCard></InfoCard>
 				<InfoCard></InfoCard>
-			</div>
-			<div className="card-row">
 				<InfoCard></InfoCard>
 				<InfoCard></InfoCard>
-			</div>
-			<div className="card-row">
+				<InfoCard></InfoCard>
+				<InfoCard></InfoCard>
+				<InfoCard></InfoCard>
+				<InfoCard></InfoCard>
 				<InfoCard></InfoCard>
 				<InfoCard></InfoCard>
 			</div>

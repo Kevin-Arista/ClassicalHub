0ebcb7f2b16b1452fc97e1a243881d66cb2f6418
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-17 14:59:22 -0500

    fixed margins and styling

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,10 +1,83 @@
 import React from "react";
+import "./events.css";
+import EventCard from "../components/eventCard";
 
-function Events(props) {
+const Events = (props) => {
+	const events = [
+		{
+			name: "Music Festival",
+			date: "2024-11-20",
+			time: "10:00 AM",
+			location: "New York",
+		},
+		{
+			name: "Art Workshop",
+			date: "2024-11-21",
+			time: "2:00 PM",
+			location: "Los Angeles",
+		},
+		{
+			name: "Tech Conference",
+			date: "2024-11-25",
+			time: "9:00 AM",
+			location: "San Francisco",
+		},
+		{
+			name: "Music Festival",
+			date: "2024-11-20",
+			time: "10:00 AM",
+			location: "New York",
+		},
+		{
+			name: "Art Workshop",
+			date: "2024-11-21",
+			time: "2:00 PM",
+			location: "Los Angeles",
+		},
+		{
+			name: "Tech Conference",
+			date: "2024-11-25",
+			time: "9:00 AM",
+			location: "San Francisco",
+		},
+		{
+			name: "Music Festival",
+			date: "2024-11-20",
+			time: "10:00 AM",
+			location: "New York",
+		},
+		{
+			name: "Art Workshop",
+			date: "2024-11-21",
+			time: "2:00 PM",
+			location: "Los Angeles",
+		},
+		{
+			name: "Tech Conference",
+			date: "2024-11-25",
+			time: "9:00 AM",
+			location: "San Francisco",
+		},
+	];
+
 	return (
-		<div className="Events">
-			<h1>Events Page</h1>
+		<div className="events">
+			<div style={{ height: "11vh" }}></div>
+			<h1>Upcoming Events:</h1>
+			<div className="events-container">
+				<div className="card-container">
+					{events.map((event, index) => (
+						<EventCard
+							key={index}
+							eventName={event.name}
+							eventDate={event.date}
+							eventLocation={event.location}
+							eventTime={event.time}></EventCard>
+					))}
+				</div>
+			</div>
 		</div>
 	);
-}
+};
+
 export default Events;

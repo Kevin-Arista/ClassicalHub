a4a5173daba249c019fbf6bec2d5607406038f14
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-19 12:06:07 -0500

    populated object containing nonprofits info, fixed style accordingly

--- src/components/infoCard.js
+++ src/components/infoCard.js
@@ -7,21 +7,20 @@ function InfoCard({
 	orgName = "Lorem",
 	description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat risus sed risus pharetra, vitae blandit purus suscipit. Integer sagittis dictum venenatis. Fusce felis felis, sollicitudin eu magna vel, laoreet porta quam. Mauris at pharetra lorem. Nulla enim mi, consequat at neque at, suscipit blandit risus. Donec consectetur nisi cursus urna tristique, eu dictum augue luctus.",
 	link = "/locations",
-	phone = "Not Available",
 }) {
 	return (
 		<div className="infoCard">
 			<h1>{orgName}</h1>
 			<hr />
-			<p>{description}</p>
+			<p className="description">{description}</p>
+			<p className="connect">
+				<strong>Connect: </strong>
+			</p>
+
 			<div className="icon-row">
-				<a href={link}>
+				<a href={link} target="_blank">
 					<img src={linkIcon} />
 				</a>
-				<div className="tooltip-container">
-					<img src={phoneIcon} alt="Phone Icon" />
-					<span className="tooltip">{phone}</span>
-				</div>
 			</div>
 		</div>
 	);
--- src/pages/locations.js
+++ src/pages/locations.js
@@ -5,40 +5,64 @@ import InfoCard from "../components/infoCard";
 function Locations(props) {
 	const nonProfits = [
 		{
-			name: "",
-			description: "",
-			link: "",
-			phone: "",
+			name: "YOLA at HOLA",
+			description:
+				"Youth Orchestra of Los Angeles (YOLA) at Heart of Los Angeles (HOLA) classes include music creativity, singing and solfège, ensemble rehearsals, and an hour of academic tutoring daily. YOLA at HOLA is a partnership between the LA Phil and Heart of Los Angeles (HOLA).",
+			link: "https://www.laphil.com/learn/yola/youth-orchestra-los-angeles",
 		},
 		{
-			name: "",
-			description: "",
-			link: "",
-			phone: "",
+			name: "YOLA at Torres",
+			description:
+				"Found inside Esteban E. Torres High School performing arts magnet school in East L.A., YOLA at Torres offers students the opportunity to thrive in an intensive, nurturing musical environment, receiving up to 15 hours of musical instruction and academic tutoring per week",
+			link: "https://www.laphil.com/learn/yola/youth-orchestra-los-angeles",
 		},
 		{
-			name: "",
-			description: "",
-			link: "",
-			phone: "",
+			name: "YOLA at Inglewood",
+			description:
+				"YOLA at Inglewood is housed at the new Beckmen YOLA Center, which emerged out of a close collaboration between the city of Inglewood, architect Frank Gehry, and LA Phil Music and Artistic Director Gustavo Dudamel. It has grown to include three orchestras, mentorship, group lessons, chamber music, and parent programs.",
+			link: "https://www.laphil.com/learn/yola/youth-orchestra-los-angeles",
 		},
 		{
-			name: "",
-			description: "",
-			link: "",
-			phone: "",
+			name: "YOLA at Camino Nuevo",
+			description:
+				"YOLA at Camino Nuevo is the first in-school YOLA model, allowing the LA Phil to bring rigorous music education into the school day and extend it after school. Currently serving students in grades K-10, YOLA at Camino has grown to three orchestras receiving up to 14 hours of musical instruction per week.",
+			link: "https://www.laphil.com/learn/yola/youth-orchestra-los-angeles",
 		},
 		{
-			name: "",
-			description: "",
-			link: "",
-			phone: "",
+			name: "Los Angeles Youth Orchestra",
+			description:
+				"The Los Angeles Youth Orchestra is a diverse group of young musicians from over sixty different public and private schools throughout the greater Los Angeles area. LAYO brings together pre-college age musicians, ages 8-18, to rehearse and perform classical symphonic masterworks and contemporary music.",
+			link: "https://losangelesyouthorchestra.org/",
 		},
 		{
-			name: "",
-			description: "",
-			link: "",
-			phone: "",
+			name: "Harmony Project at Expo Center",
+			description:
+				"Harmony Project uses the transformative power of music to open up life-changing experiences to students in under-resourced communities, and seeks to maximize their opportunities for success. Serve students ages 6 to 18 and have two full-sized orchestras, as well as wind and string ensembles. ",
+			link: "https://www.harmony-project.org/",
+		},
+		{
+			name: "Pasadena Youth Symphony Orchestra",
+			description:
+				"The Pasadena Youth Symphony Orchestras (PYSO) have continually offered robust musical training and personal development for young musicians throughout the San Gabriel Valley for more than 40 years. This prestigious program is designed specifically for students between the ages of 11 and 18.",
+			link: "https://pasadenasymphony-pops.org/education/",
+		},
+		{
+			name: "Inner City Youth Orchestra Los Angeles",
+			description:
+				"ICYOLA transforms the lives and minds of young people in Inner City Los Angeles through high quality music education. We teach inner-city youth the great music of the world and provide opportunities for them to perform that music in the most magnificent settings of our community.",
+			link: "https://icyola.org/",
+		},
+		{
+			name: "Sounds like LA at Santa Monica",
+			description:
+				"Sounds Like LA offers tuition-free music education and public concerts to diverse communities in the LA area, harnessing the power of music to bridge economic and cultural differences and to foster personal and social change.",
+			link: "https://soundslikela.org/",
+		},
+		{
+			name: "Glendale Youth Symphony",
+			description:
+				"The Glendale Youth Symphony mission is to foster and cultivate the highest level of performance of orchestral music in students living in Los Angeles County through student-centered mentorship and instruction. ",
+			link: "https://glendaleyouthsymphony.org/",
 		},
 	];
 
@@ -51,15 +75,14 @@ function Locations(props) {
 				src="https://www.google.com/maps/d/u/7/embed?mid=1NIg4BgSx5HuC3NKGj6psfsecvW4yDqg&ehbc=2E312F&noprof=1"
 				width="640"
 				height="480"></iframe>
-			<h1>Learn More Below!</h1>
+			<h1 className="learnmore">Learn More Below!</h1>
 			<div className="info-cards-container">
 				{nonProfits.map((org, index) => (
 					<InfoCard
 						key={index}
 						orgName={org.name}
 						description={org.description}
-						link={org.link}
-						phone={org.phone}></InfoCard>
+						link={org.link}></InfoCard>
 				))}
 			</div>
 		</div>

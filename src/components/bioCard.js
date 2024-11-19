2b46c868a687c5e71e02866773e488fb9c4e2448
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-19 11:19:32 -0500

    implemented image and bio, fixed style accordingly

--- src/components/bioCard.js
+++ src/components/bioCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./bioCard.scss";
+import bioPic from "../assets/johnPic.png";
 
 function BioCard(props) {
 	return (
@@ -9,19 +10,22 @@ function BioCard(props) {
 					<h1>Hello, </h1>
 					<h1>My name is John!</h1>
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamc. Lorem ipsum dolor
-						sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-						incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-						veniam, quis nostrud exercitation ullamc
+						John Gonzalez was born and raised in Los Angeles, where he
+						discovered his passion for music as a young musician in the Los
+						Angeles Philharmonic’s youth orchestra program: Youth Orchestra of
+						Los Angeles (YOLA). Over the course of seven years in YOLA, John
+						developed his skills and deepened his love for the arts, and it is
+						where music went from a hobby to his biggest passion. Currently,
+						John is in his last year studying bassoon performance at the USC
+						Thornton School of Music, where he is mentored by Shawn Mouser,
+						associate principal bassoon of the Los Angeles Philharmonic. Through
+						this website, John hopes to inspire the next generation of musicians
+						to pursue their passions and explore the transformative power of
+						classical music.
 					</p>
 				</div>
 				<div className="vl"></div>
-				<img
-					className="bioPicture"
-					alt="profile_pic"
-					src="https://thenoblepaw.com/wp-content/uploads/2022/03/Dachshund.jpg"></img>
+				<img className="bioPicture" alt="profile_pic" src={bioPic}></img>
 			</div>
 		</div>
 	);

dfb02f0720bf9e2fa29b7b797f81c874bf700c84
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-23 01:15:03 -0500

    used object structured props, modified calling convention, presetted value NULL

--- src/components/eventCard.js
+++ src/components/eventCard.js
@@ -2,18 +2,23 @@ import React from "react";
 import "./eventCard.css";
 import { FaCalendarAlt, FaClock, FaMapMarkerAlt } from "react-icons/fa";
 
-function EventCard(props) {
+function EventCard({
+	eventName = "NULL",
+	eventDate = "NULL",
+	eventTime = "NULL",
+	eventLocation = "NULL",
+}) {
 	return (
 		<div className="event-card">
-			<p className="event-name">{props.eventName}</p>
+			<p className="event-name">{eventName}</p>
 			<p className="event-date">
-				<FaCalendarAlt /> {new Date(props.eventDate).toLocaleDateString()}
+				<FaCalendarAlt /> {new Date(eventDate).toLocaleDateString()}
 			</p>
 			<p className="event-time">
-				<FaClock /> {props.eventTime}
+				<FaClock /> {eventTime}
 			</p>
 			<p className="event-location">
-				<FaMapMarkerAlt /> {props.eventLocation}
+				<FaMapMarkerAlt /> {eventLocation}
 			</p>
 		</div>
 	);

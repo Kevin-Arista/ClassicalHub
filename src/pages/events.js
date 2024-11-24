ee48129062fb0a33ae8175293ead8e7584cfecd6
Kevin Arista S <kevin.a8988@gmail.com>
2024-11-24 16:57:44 -0500

    implemented filter function to remove past events

--- src/pages/events.js
+++ src/pages/events.js
@@ -4,11 +4,20 @@ import EventCard from "../components/eventCard";
 import events from "../db/event-db.js";
 const Events = (props) => {
 	events.sort((a, b) => {
+		if (a.date.slice(0, 5) < b.date.slice(0, 5)) return -1;
+		if (a.date.slice(0, 5) > b.date.slice(0, 5)) return 1;
+
 		if (a.date.slice(5, 7) < b.date.slice(5, 7)) return -1;
 		if (a.date.slice(5, 7) > b.date.slice(5, 7)) return 1;
 
 		if (a.date.slice(8) < b.date.slice(8)) return -1;
 		if (a.date.slice(8) > b.date.slice(8)) return 1;
+	});
+
+	const upcomingEvents = events.filter((event) => {
+		const now = new Date();
+		const eventDate = new Date(event.date + "T00:00:00-08:00");
+		return eventDate >= now;
 	});
 
 	return (
@@ -17,7 +26,7 @@ const Events = (props) => {
 			<h1>Upcoming Events:</h1>
 			<div className="events-container">
 				<div className="card-container">
-					{events.map((event, index) => (
+					{upcomingEvents.map((event, index) => (
 						<EventCard
 							data-testid="event-card"
 							key={index}
